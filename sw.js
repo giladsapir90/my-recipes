7ef4ce60fbbd46c8fe90b33ad90afdcf2a2384fb
giladsapir90 <giladsapir90@gmail.com>
2026-03-07 20:51:58 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'recipes-v1';
+const ASSETS = ['/my-recipes/', '/my-recipes/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
